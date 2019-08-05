32a015df6b2f1416f61aa44d29fa8e916abd5175
Risper Akinyi <email@example.com>
2019-08-05 11:56:06 +0300

    intialize the SQLAlchemy extension

--- app/models.py
+++ app/models.py
@@ -31,7 +31,7 @@ class User(UserMixin,db.Model):
         return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
-        return f'User {self.username}'cshdvcd
+        return f'User {self.username}'
 
 class Pitch(db.Model):
 
